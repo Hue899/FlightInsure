dbe3d1c7268980240c9e6b8ee139ad94b76968a7
Hue <67720949+Hue899@users.noreply.github.com>
2022-07-17 17:17:18 -0700

    First commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function(deployer) {
+
+    let firstAirline = '0x3D0c5d2Acf7Da7106806b1a74CB7e34FB29354D9';
+    deployer.deploy(FlightSuretyData, firstAirline)
+    .then(() => {
+        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:9545',
+                            dataAddress: FlightSuretyData.address,
+                            appAddress: FlightSuretyApp.address
+                        }
+                    }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+    });
+}
---
+++ webpack.config.dapp.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ['babel-polyfill', path.join(__dirname, "src/dapp")],
+  output: {
+    path: path.join(__dirname, "prod/dapp"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+    {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: path.join(__dirname, "src/dapp/index.html")
+    })
+  ],
+  resolve: {
+    extensions: [".js"],
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "os": require.resolve("os-browserify/browser"),
+      "crypto-browserify": require.resolve('crypto-browserify') //if you want to use this module also don't forget npm i crypto-browserify 
+    }
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dapp"),
+    port: 8000,
+    //stats: "minimal"
+  }
+};
